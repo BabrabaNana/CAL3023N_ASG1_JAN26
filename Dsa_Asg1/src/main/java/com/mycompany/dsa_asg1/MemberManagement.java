2e6c5c4acdfb0172e67ed4940c22d542088e2db5
DC <yewchuanteh@gmail.com>
2026-03-08 20:26:09 +0800

    dc

--- Dsa_Asg1/src/main/java/com/mycompany/dsa_asg1/MemberManagement.java
+++ Dsa_Asg1/src/main/java/com/mycompany/dsa_asg1/MemberManagement.java
@@ -3,8 +3,48 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.dsa_asg1;
-
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeParseException;
 
 public class MemberManagement {
+    private Scanner scanner;
+    private LinkedList<Member> memberList = new LinkedList<>();
+    
+    // Add member
+    public void registerMember(){
+        
+        String memberId; //No need setter (unique identifier)
+        String name;
+        String dateOfBirth;
+        int age;
+        String gender;
+        String contactNumber;
+        String address;
+        String membershipLevel;
+        String dateOfJoining;
+        String membershipStatus;
+        String expiryDate;
+        
+        System.out.println("\n=== Register New Member ===");
+        
+        // Validation
+        
+    
+    
+    
+    
+    }
+    
+    
+    // Validation method
+    
+    
+    
+    
+    
+    
     
 }
